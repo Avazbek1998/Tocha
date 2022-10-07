3e49b54172f41aa302292b3d42c9a4a5db3abd13
Avazbek1998 <citytashkent6@gmail.com>
2022-10-07 21:45:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu__burger');
+
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    
+})
